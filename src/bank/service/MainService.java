f859d7e394aeaba202acdefd94a97ed1dd207591
Jedyn <zar1117@o2.pl>
2022-04-26 20:09:55 +0200

    init

--- src/bank/service/MainService.java
+++ src/bank/service/MainService.java
@@ -6,6 +6,7 @@ import bank.repository.MainRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainService {
     private MainRepository repository; // TODO zrobić final
@@ -43,12 +44,22 @@ public class MainService {
     public void wyswietlStanKonta(Uzytkownik wlasciciel) {
         List<KontoBankowe> kontaBankowe = repository.pobierzWszystkieKonta();
         List<KontoBankowe> wynik = new ArrayList<>();
-        for (int i = 0; i < kontaBankowe.size(); i++) {
-            if(czyTenSamWlasciciel(wlasciciel, kontaBankowe.get(i).getWlasciciel())){
-
+        for (KontoBankowe kontoBankowe : kontaBankowe) {
+            if (czyTenSamWlasciciel(wlasciciel, kontoBankowe.getWlasciciel())) {
+                wynik.add(kontoBankowe);
             }
         }
 
+        if(wynik.size() == 1){
+            System.out.println("Twój stan konta to: " + wynik.get(0).getBalans() + " zł");
+        } else {
+            System.out.println("Wybierz numer konta z którego wyświetlić saldo: ");
+            for (int i = 0; i < wynik.size(); i++) {
+                System.out.println((i+1) + ". " + wynik.get(i));
+            }
+            int wybor = new Scanner(System.in).nextInt();
+            System.out.println("Twój stan konta to: " + wynik.get((wybor-1)).getBalans() + " zł");
+        }
     }
 
     private boolean czyTenSamWlasciciel(Uzytkownik wlasciciel, Uzytkownik wlasciciel2) {
